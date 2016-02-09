83bee86db05211de8808588ae0e2f507262850cd
Perry R Eising <perry.r.eising@gmail.com>
2016-02-09 09:07:52 -0800

    add loop to create scoring options for whole words

--- src/main/java/ScrabbleApp.java
+++ src/main/java/ScrabbleApp.java
@@ -16,48 +16,52 @@ public class ScrabbleApp {
         get("/scrabbleoutput", (request, response) -> {
             HashMap model = new HashMap();
             String userInput = request.queryParams("userInput").toUpperCase();
+            char[] letterArray = userInput.toCharArray();
+
             model.put("userInput", userInput);
 
-            HashMap <String, Integer> scores = new HashMap<>();
+            HashMap <Character, Integer> scores = new HashMap<>();
 
-            scores.put("A", 1);
-            scores.put("E", 1);
-            scores.put("I", 1);
-            scores.put("O", 1);
-            scores.put("U", 1);
-            scores.put("L", 1);
-            scores.put("N", 1);
-            scores.put("R", 1);
-            scores.put("S", 1);
-            scores.put("T", 1);
+            scores.put('A', 1);
+            scores.put('E', 1);
+            scores.put('I', 1);
+            scores.put('O', 1);
+            scores.put('U', 1);
+            scores.put('L', 1);
+            scores.put('N', 1);
+            scores.put('R', 1);
+            scores.put('S', 1);
+            scores.put('T', 1);
 
-            scores.put("D", 2);
-            scores.put("G", 2);
+            scores.put('D', 2);
+            scores.put('G', 2);
 
-            scores.put("B", 3);
-            scores.put("C", 3);
-            scores.put("M", 3);
-            scores.put("P", 3);
+            scores.put('B', 3);
+            scores.put('C', 3);
+            scores.put('M', 3);
+            scores.put('P', 3);
 
-          	scores.put("F", 4);
-          	scores.put("H", 4);
-          	scores.put("V", 4);
-          	scores.put("W", 4);
-          	scores.put("Y", 4);
+          	scores.put('F', 4);
+          	scores.put('H', 4);
+          	scores.put('V', 4);
+          	scores.put('W', 4);
+          	scores.put('Y', 4);
 
-          	scores.put("K", 5);
+          	scores.put('K', 5);
 
-          	scores.put("J", 8);
-          	scores.put("X", 5);
+          	scores.put('J', 8);
+          	scores.put('X', 5);
 
-			      scores.put("Q", 10);
-			      scores.put("Z", 10);
+			      scores.put('Q', 10);
+			      scores.put('Z', 10);
 
-       		  Integer letterValue = scores.get(userInput);
-            System.out.println(letterValue);
-            model.put("letterValue", letterValue);
+            Integer scoreKeeper = 0;
 
-            //this works perfectly except it won't output the correct number when the variable is used.
+            for (char letter : letterArray){
+              Integer letterValue = scores.get(letter);
+              scoreKeeper += letterValue;
+            }
+            model.put("scoreKeeper", scoreKeeper);
 
             return new ModelAndView(model, "templates/scrabbleoutput.vtl");
         }, new VelocityTemplateEngine());
